1ee2bc91079414a87c83aaef13c03ff527995f66
Nils Tarjei Hjelme <nils@isi.no>
2016-11-04 11:53:57 +0100

    Added helper class to move data between c++ and v8

---
+++ DataHelper.cc
@@ -0,0 +1,74 @@
+#include "DataHelper.h"
+
+
+Local<Object> DataHelper::getV8Data(std::map <std::string, ItemValue> doc) {
+	Local<Object> resDoc = Nan::New<Object>();
+	std::map<std::string, ItemValue>::iterator it;
+	for (it = doc.begin(); it != doc.end(); it++)
+	{
+		ItemValue value = it->second;
+		if (value.type == 0) {
+			Nan::Set(resDoc, New<String>(it->first).ToLocalChecked(), New<Number>(value.numberValue));
+		}
+		else if (value.type == 1) {
+			Nan::Set(resDoc, New<v8::String>(it->first).ToLocalChecked(), New<v8::String>(value.stringValue).ToLocalChecked());
+						}
+		else if (value.type == 2) {
+			int ii;
+			Local<Array> arr = New<Array>(value.vectorStrValue.size());
+			for (ii = 0; ii < value.vectorStrValue.size(); ii++) {
+				Nan::Set(arr, ii, Nan::New<String>(value.vectorStrValue[ii]).ToLocalChecked());
+			}
+			Nan::Set(resDoc, New<v8::String>(it->first).ToLocalChecked(), arr);
+
+		}
+		else if (value.type == 3) {
+			Nan::Set(resDoc, New<v8::String>(it->first).ToLocalChecked(), New<v8::Date>(value.dateTimeValue).ToLocalChecked());
+		}
+	}
+
+	
+	return resDoc;
+}
+
+Local<Array> DataHelper::getV8Data(std::vector <std::map<std::string, ItemValue>> viewResult) {
+
+	Local<Array> viewRes = New<Array>(viewResult.size());
+	try {
+		int j;
+		for (j = 0; j < viewResult.size(); j++) {
+			std::map<std::string, ItemValue> doc = viewResult[j];
+			Local<Object> resDoc = Nan::New<Object>();
+			std::map<std::string, ItemValue>::iterator it;
+			for (it = doc.begin(); it != doc.end(); it++)
+			{
+				ItemValue value = it->second;
+				if (value.type == 0) {
+					Nan::Set(resDoc, New<String>(it->first).ToLocalChecked(), New<Number>(value.numberValue));
+				}
+				else if (value.type == 1) {
+					Nan::Set(resDoc, New<v8::String>(it->first).ToLocalChecked(), New<v8::String>(value.stringValue).ToLocalChecked());
+
+				}
+				else if (value.type == 2) {
+					int ii;
+					Local<Array> arr = New<Array>(value.vectorStrValue.size());
+					Nan::Set(arr, 0, Nan::New<String>("").ToLocalChecked());
+					for (ii = 0; ii < value.vectorStrValue.size(); ii++) {
+						Nan::Set(arr, ii, Nan::New<String>(value.vectorStrValue[ii]).ToLocalChecked());
+					}
+					Nan::Set(resDoc, New<v8::String>(it->first).ToLocalChecked(), arr);
+
+				}
+			}
+			Nan::Set(viewRes, j, resDoc);
+		}
+	}
+	catch (LNSTATUS Lnerror) {
+		char ErrorBuf[512];
+		LNGetErrorMessage(Lnerror, ErrorBuf, 512);
+		std::cout << "Handle Error:  " << ErrorBuf << std::endl;
+	}
+	return viewRes;
+
+}
---
+++ DataHelper.h
@@ -0,0 +1,56 @@
+/*******************************************************************************
+* Domino addon for Node.js
+*
+* Copyright (c) 2016 Nils T. Hjelme
+*
+* The MIT License (MIT)
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*******************************************************************************/
+
+#ifndef DATA_HELPER_H_
+#define DATA_HELPER_H_
+
+#include <nan.h>
+#include <ctime>
+#include <time.h>
+#include <iostream>
+#include <map>
+#include <iterator>
+#include <vector>
+
+#include "ItemValue.h"
+
+using v8::Local;
+using v8::Array;
+using v8::Object;
+using v8::Number;
+using v8::Value;
+using v8::String;
+using Nan::New;
+using Nan::Null;
+using Nan::To;
+
+class DataHelper {
+public:
+	static Local<Object> getV8Data(std::map <std::string, ItemValue> doc);
+	static Local<Array> getV8Data(std::vector <std::map<std::string, ItemValue>> viewResult);
+};
+
+#endif  // ITEM_VALUE_H_
