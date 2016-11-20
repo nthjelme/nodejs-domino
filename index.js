204849d609e66dccd4e9c23847a48d54cb495b81
Nils Tarjei Hjelme <nhjelme@gmail.com>
2016-11-20 20:19:58 +0100

    feat(js module): added termSession function

--- index.js
+++ index.js
@@ -4,17 +4,21 @@ var dominoDriver = require('./build/Release/addon');
 function Domino() {
 	dominoDriver.initSession();
 	var dbObj = {};
+	var termSession = function() {
+		dominoDriver.termSession();
+	};
+
 	var use = function(db) {
 		var localDb = db;
-		
+
 		var replicate = function(callback) {
 			callback({error:"Not implemented"},undefined);
 			/*dominoDriver.replicateAsync(db,function(error,result) {
 				callback(error,result);
 			});*/
 		}
-		
-		var get = function(unid,callback) {			
+
+		var get = function(unid,callback) {
 			dominoDriver.getDocumentAsync(localDb,unid,function(error,document) {
 				if (document) {
 					document.getResponses = function(responseCallback) {
@@ -28,7 +32,7 @@ function Domino() {
 						});
 					}
 					document.save = function(documentCallback) {
-						dominoDriver.saveDocumentAsync(localDb,this,function(error,document) {							
+						dominoDriver.saveDocumentAsync(localDb,this,function(error,document) {
 							documentCallback(error,document);
 						});
 
@@ -37,25 +41,25 @@ function Domino() {
 				callback(error,document);
 			});
 		}
-		
+
 		var insert = function(document,callback) {
 			dominoDriver.saveDocumentAsync(localDb,document,function(error,document) {
 				callback(error,document);
 			});
 		};
-		
+
 		var deleteFn = function(unid,callback) {
 			dominoDriver.deleteDocumentAsync(localDb,unid,function(error,result) {
 				callback(error,result);
 			});
 		};
-		
+
 		var makeResponse = function(doc,parent,callback) {
 			dominoDriver.makeResponseDocumentAsync(localDb,doc["@unid"], parent["@unid"], function(error,result) {
 					callback(error,result);
 			});
 		};
-		
+
 		var getResponses = function(doc,callback) {
 			callback({error: "Not implemented"},undefined)
 			/*
@@ -63,21 +67,22 @@ function Domino() {
 					callback(error,result);
 			});*/
 		};
-		
+
 		var view = function(view,callback) {
 			dominoDriver.getViewAsync(localDb,view,function(err,result) {
 				callback(err,result);
 			});
 		};
-		
+
 		localDb.get = get;
 		localDb.insert = insert;
 		localDb.del = deleteFn;
 		localDb.view = view;
 		localDb.replicate = replicate;
 		localDb.makeResponse = makeResponse;
+
 		return localDb;
-			
+
 	};
 	
 	process.on('SIGINT', function() {
@@ -86,6 +91,7 @@ function Domino() {
 	});
 	
 	dbObj.use = use;
-	
+	dbObj.termSession = termSession;
+
 	return dbObj;
 };
